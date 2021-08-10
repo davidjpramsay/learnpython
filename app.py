93ddc243b33b2e5556a630edff71ba7797a9acb0
davidjpramsay <david.jp.ramsay@me.com>
2021-08-10 16:26:00 +0800

    Variables Lesson Commit

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("Hello World")
+character_name = "John"
+chacter_age = "35"
+print("There was once an old man named " + character_name + ", ")
+print("he was " + chacter_age + " years old.")
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + chacter_age + ".")
